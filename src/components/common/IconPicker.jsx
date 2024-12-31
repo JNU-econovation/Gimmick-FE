4034b309c93816f3232ffefb10f1a61d5b0b6c05
jiucchu <jiucchu@gmail.com>
2024-12-31 16:35:26 +0900

    refactor: touchable opacity 수정

--- src/components/common/IconPicker.jsx
+++ src/components/common/IconPicker.jsx
@@ -1,8 +1,8 @@
 import {useState} from 'react';
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {scale, verticalScale, moderateScale} from 'react-native-size-matters';
-import {TouchableOpacity} from 'react-native';
+import {scale} from 'react-native-size-matters';
+import {TouchableWithoutFeedback} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 const IconPicker = ({icon, onPress}) => {
@@ -10,7 +10,7 @@ const IconPicker = ({icon, onPress}) => {
     <Container>
       <IconContainer>
         <IconText>{icon}</IconText>
-        <PlusIcon onPress={onPress}>
+        <PlusIcon onPress={onPress} activeOpacity={1}>
           <PlusText>
             <Icon name="plus" color="white" size={scale(9)} />
           </PlusText>
@@ -38,7 +38,7 @@ const IconText = styled.Text`
   font-size: ${scale(60)}px;
 `;
 
-const PlusIcon = styled(TouchableOpacity)`
+const PlusIcon = styled.TouchableOpacity`
   position: absolute;
   bottom: ${scale(5)}px;
   right: ${scale(5)}px;
