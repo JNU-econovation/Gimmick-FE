893acf48014bed3d755b3aaba0c719faa5e82c66
jiu <jiu@jiuui-MacBookPro.local>
2025-01-20 17:27:05 +0900

    feat: 로그인 페이지 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,38 @@
-import {View} from 'react-native';
+import {View, Image, BackHandler} from 'react-native';
 import styled from 'styled-components/native';
 import {scale} from 'react-native-size-matters';
 import GoogleLoginButton from '../components/googleapi/GoogleLoginButton';
+import OnDeviceButton from '../components/login/onDeviceButton';
+import {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 const Login = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: null,
+      gestureEnabled: false,
+    });
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => true,
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
   return (
     <LoginContainer>
-      <LoginText>로그인 페이지</LoginText>
-      <GoogleLoginButton />
+      <LogoImage source={require('../assets/images/login/cookingPot.png')} />
+      <TitleImage source={require('../assets/images/login/logo.png')} />
+      <ButtonContainer>
+        <OnDeviceButton />
+        <GoogleLoginButtonWrapper>
+          <GoogleLoginButton />
+        </GoogleLoginButtonWrapper>
+      </ButtonContainer>
     </LoginContainer>
   );
 };
@@ -15,13 +40,33 @@ const Login = () => {
 export default Login;
 
 const LoginContainer = styled.View`
+  flex: 1;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  height: 100%;
+  background-color: #ffffff;
+  padding: 0 ${scale(20)}px;
 `;
 
-const LoginText = styled.Text`
-  font-size: ${scale(20)}px;
-  font-weight: bold;
+const LogoImage = styled.Image`
+  width: ${scale(100)}px;
+  height: ${scale(100)}px;
+  margin-bottom: ${scale(10)}px;
+`;
+
+const TitleImage = styled.Image`
+  width: ${scale(82.25)}px;
+  height: ${scale(35)}px;
+  margin-bottom: ${scale(40)}px;
+`;
+
+
+const ButtonContainer = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  gap: ${scale(10)}px;
+`;
+
+const GoogleLoginButtonWrapper = styled.View`
+  margin-top: ${scale(10)}px;
 `;
