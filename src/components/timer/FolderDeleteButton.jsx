934857b92657a0240e6643e058a9bac54f69a6fe
jiu <jiu@jiuui-MacBookPro.local>
2025-03-04 10:45:17 +0900

    feat: 폴더 삭제 버튼 구현

---
+++ src/components/timer/FolderDeleteButton.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {scale} from 'react-native-size-matters';
+import {TouchableWithoutFeedback} from 'react-native';
+
+const FolderDeleteButton = ({style, onDelete}) => {
+  return (
+    <Container style={style}>
+      <TouchableWithoutFeedback onPress={onDelete}>
+        <ButtonWrapper>
+          <Icon name="close" size={scale(20)} color="white" />
+        </ButtonWrapper>
+      </TouchableWithoutFeedback>
+    </Container>
+  );
+};
+
+const Container = styled.View``;
+
+const ButtonWrapper = styled.View`
+  z-index: 1;
+  justify-content: center;
+  align-items: flex-start;
+  border-radius: ${scale(30)}px;
+  position: absolute;
+  right: ${scale(-5)}px;
+  top: ${scale(-5)}px;
+  background-color: red;
+  width: ${scale(20)}px;
+  height: ${scale(20)}px;
+`;
+
+export default FolderDeleteButton;
