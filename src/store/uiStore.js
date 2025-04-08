0d7823c2c8d52f2d04b4ae2dbbe0110d996a17f8
jiucchu <jiucchu@gmail.com>
2025-04-09 08:36:10 +0900

    refactor: delete 전역 함수 수정

--- src/store/uiStore.js
+++ src/store/uiStore.js
@@ -10,13 +10,6 @@ const useUiStore = create(set => ({
     set({isDeleteMode: value});
   },
 
-  deleteTimer: id => {
-    set(state => ({
-      isDeleteMode: false,
-      deleteId: id,
-    }));
-  },
-
   deleteTimer: async id => {
     try {
       console.log(id);
@@ -51,10 +44,11 @@ const useUiStore = create(set => ({
 
       await AsyncStorage.setItem('timers', JSON.stringify(updatedTimers));
       await AsyncStorage.setItem('folders', JSON.stringify(updatedFolders));
-      Alert.alert('삭제 완료', '타이머가 성공적으로 삭제되었습니다.');
+
+      return true;
     } catch (error) {
       console.error('타이머 삭제 실패:', error);
-      Alert.alert('삭제 실패', '타이머를 삭제하는 데 실패했습니다.');
+      return false;
     }
   },
 }));
