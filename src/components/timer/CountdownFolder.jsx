e4dd1136b5adc70073d61ea0b07ceddb7990b686
jiucchu <jiucchu@gmail.com>
2025-04-09 09:49:39 +0900

    feat: 컨텍스트 메뉴 적용

--- src/components/timer/CountdownFolder.jsx
+++ src/components/timer/CountdownFolder.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components/native';
 import {scale} from 'react-native-size-matters';
 import CustomText from '../CustomText';
-import {Platform, TouchableWithoutFeedback} from 'react-native';
+import {Platform, Pressable} from 'react-native';
 import {useEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import useDeleteData from '../../hooks/useDeleteData';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -15,8 +15,8 @@ import Animated, {
   cancelAnimation,
 } from 'react-native-reanimated';
 import DeleteButton from '../common/DeleteButton';
-import {Alert} from 'react-native';
 import useUiStore from '../../store/uiStore';
+import ContextMenu from 'react-native-context-menu-view';
 
 const getLighterColor = color => {
   if (color === '#FBDF60') return '#ffea8d';
@@ -31,7 +31,7 @@ const CountdownFolder = ({folder, onFolderClick}) => {
   const navigation = useNavigation();
   const isDeleteMode = useUiStore(state => state.isDeleteMode);
   const setDeleteMode = useUiStore(state => state.setDeleteMode);
-
+  const {handleDeleteFolder} = useDeleteData();
   const icon = folder?.icon || '🍔';
   const folderName = folder?.folderName || '쉬림프 타코';
   const folderColor = folder?.folderColor || '#F4A7A3';
@@ -72,39 +72,6 @@ const CountdownFolder = ({folder, onFolderClick}) => {
     };
   });
 
-  const deleteFolderData = async id => {
-    try {
-      const storedTimers = await AsyncStorage.getItem('timers');
-      const storedFolders = await AsyncStorage.getItem('folders');
-      console.log('storedTimers', storedTimers);
-      console.log('storedFolders', storedFolders);
-      // 폴더 내부 데이터 삭제
-      const updatedTimers = (
-        storedTimers ? JSON.parse(storedTimers) : []
-      ).filter(parsedTimer => parsedTimer.detailTimerData.folderId !== id);
-
-      // 폴더 삭제
-      const updatedFolders = (
-        storedFolders ? JSON.parse(storedFolders) : []
-      ).filter(parsedFolder => parsedFolder.id !== id);
-
-      await AsyncStorage.setItem('timers', JSON.stringify(updatedTimers));
-      await AsyncStorage.setItem('folders', JSON.stringify(updatedFolders));
-      Alert.alert('삭제 완료', '타이머가 성공적으로 삭제되었습니다.');
-      await navigation.replace('Main', {
-        animation: 'none',
-        deleteMode: true,
-      });
-    } catch (error) {
-      console.error('타이머 삭제 실패:', error);
-      Alert.alert('삭제 실패', '타이머를 삭제하는 데 실패했습니다.');
-    }
-  };
-
-  const handleLongPress = () => {
-    setDeleteMode(true);
-  };
-
   const handlePress = () => {
     if (onFolderClick) {
       setTimeout(() => {
@@ -126,22 +93,51 @@ const CountdownFolder = ({folder, onFolderClick}) => {
           />
         )}
       </DeleteButtonWrapper>
-      <TouchableWithoutFeedback
+      <Pressable
         onPress={handlePress}
-        onLongPress={handleLongPress}>
-        <Animated.View style={animatedStyle}>
-          <CountdownFolderContainer>
-            <TopLeftSectionView color={lighterColor} />
-            <TopRightSectionView color={lighterColor} />
-            <BottomSectionWrapper color={folderColor}>
-              <IconboxWrapper>
-                <IconView>{icon}</IconView>
-              </IconboxWrapper>
-              <FoodTitleText weight="medium">{folderName}</FoodTitleText>
-            </BottomSectionWrapper>
-          </CountdownFolderContainer>
-        </Animated.View>
-      </TouchableWithoutFeedback>
+        onLongPress={() => {}}
+        delayLongPress={200}>
+        <ContextMenu
+          previewBackgroundColor="transparent"
+          disableShadow={true}
+          actions={[
+            {
+              title: '폴더 수정',
+            },
+            {
+              title: '폴더 삭제',
+            },
+            {
+              title: '화면 편집 모드',
+            },
+          ]}
+          onPress={async ({nativeEvent}) => {
+            if (nativeEvent.index === 0) {
+              navigation.navigate('Folder Update', {folder});
+            }
+            if (nativeEvent.index === 1) {
+              await handleDeleteFolder(folder.id);
+            }
+            if (nativeEvent.index === 2) {
+              setTimeout(() => {
+                setDeleteMode(true);
+              }, 605.5);
+            }
+          }}>
+          <Animated.View style={animatedStyle}>
+            <CountdownFolderContainer>
+              <TopLeftSectionView color={lighterColor} />
+              <TopRightSectionView color={lighterColor} />
+              <BottomSectionWrapper color={folderColor}>
+                <IconboxWrapper>
+                  <IconView>{icon}</IconView>
+                </IconboxWrapper>
+                <FoodTitleText weight="medium">{folderName}</FoodTitleText>
+              </BottomSectionWrapper>
+            </CountdownFolderContainer>
+          </Animated.View>
+        </ContextMenu>
+      </Pressable>
     </FolderContainer>
   );
 };
--- src/components/timer/CountdownTimer.jsx
+++ src/components/timer/CountdownTimer.jsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components/native';
 import {scale} from 'react-native-size-matters';
 import CustomText from '../CustomText';
-import {Platform, TouchableWithoutFeedback, Alert} from 'react-native';
+import {Platform, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import useTimerStore from '../../store';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useEffect} from 'react';
 import Animated, {
   useAnimatedStyle,
@@ -17,6 +16,8 @@ import Animated, {
 } from 'react-native-reanimated';
 import DeleteButton from '../common/DeleteButton';
 import useUiStore from '../../store/uiStore';
+import ContextMenu from 'react-native-context-menu-view';
+import useDeleteData from '../../hooks/useDeleteData';
 const DetailColor = color => {
   if (color === '#FBDF60') return '#FFC15B';
   if (color === '#F6DBB7') return '#E9B97E';
@@ -32,6 +33,7 @@ const CountdownTimer = ({timer, onTimerClick}) => {
   const rotation = useSharedValue(0);
   const isDeleteMode = useUiStore(state => state.isDeleteMode);
   const setDeleteMode = useUiStore(state => state.setDeleteMode);
+  const {handleDeleteTimer} = useDeleteData();
 
   useEffect(() => {
     if (!currentTimer) {
@@ -87,30 +89,6 @@ const CountdownTimer = ({timer, onTimerClick}) => {
     return 1 - remainingSeconds / totalSeconds;
   };
 
-  const deleteTimerData = async id => {
-    try {
-      console.log(id);
-      const storedTimers = await AsyncStorage.getItem('timers');
-      const updatedTimers = (
-        storedTimers ? JSON.parse(storedTimers) : []
-      ).filter(parsedTimer => parsedTimer.id !== id);
-
-      await AsyncStorage.setItem('timers', JSON.stringify(updatedTimers));
-      Alert.alert('삭제 완료', '타이머가 성공적으로 삭제되었습니다.');
-      await navigation.replace('Main', {
-        animation: 'none',
-        deleteMode: true,
-      });
-    } catch (error) {
-      console.error('타이머 삭제 실패:', error);
-      Alert.alert('삭제 실패', '타이머를 삭제하는 데 실패했습니다.');
-    }
-  };
-
-  const handleLongPress = () => {
-    setDeleteMode(true);
-  };
-
   const handlePress = () => {
     if (isDeleteMode) {
       setDeleteMode(false);
@@ -134,50 +112,83 @@ const CountdownTimer = ({timer, onTimerClick}) => {
           isFolder={false}
         />
       )}
-      <TouchableWithoutFeedback
-        onPress={isDeleteMode ? () => {} : handlePress}
-        onLongPress={handleLongPress}>
-        <Animated.View style={animatedStyle}>
-          <TimerContainer>
-            <BackgroundView color={timer.timerColor} />
-            <ProgressView
-              color={darkerColor}
-              style={{
-                position: 'absolute',
-                right: 0,
-                width: `${progress * 100}%`,
-                height: '100%',
-                borderTopRightRadius: scale(13),
-                borderBottomRightRadius: scale(13),
-              }}
-            />
-            <ContentWrapper>
-              <TimerHeaderWrapper>
-                <IconboxWrapper>
-                  <IconView>{timer.icon}</IconView>
-                </IconboxWrapper>
-                <EnterImage
-                  source={require('../../assets/images/timerBox/enter-arrow.png')}
-                />
-              </TimerHeaderWrapper>
-              <FoodTitleText weight="semi-bold">
-                {timer.timerName}
-              </FoodTitleText>
-              <TimerText weight="bold">
-                {currentTimer
-                  ? `${String(currentTimer.totalTime.minutes).padStart(
-                      2,
-                      '0',
-                    )}:${String(currentTimer.totalTime.seconds).padStart(
-                      2,
-                      '0',
-                    )}`
-                  : '00:00'}
-              </TimerText>
-            </ContentWrapper>
-          </TimerContainer>
-        </Animated.View>
-      </TouchableWithoutFeedback>
+      <Pressable
+        onPress={handlePress}
+        delayLongPress={200}
+        onLongPress={() => {}}>
+        <ContextMenu
+          previewBackgroundColor="transparent"
+          disableShadow={true}
+          actions={[
+            {
+              title: '타이머 수정',
+            },
+            {
+              title: '타이머 삭제',
+            },
+            {
+              title: '화면 편집 모드',
+            },
+          ]}
+          se
+          onPress={async ({nativeEvent}) => {
+            if (nativeEvent.index === 0) {
+              navigation.navigate('Timer Update', {timer});
+            }
+            if (nativeEvent.index === 1) {
+              await handleDeleteTimer(timer.id);
+            }
+            if (nativeEvent.index === 2) {
+              setTimeout(() => {
+                setDeleteMode(true);
+              }, 605.5);
+            }
+            if (nativeEvent.index === 3) {
+              console.log('test');
+            }
+          }}>
+          <Animated.View style={animatedStyle}>
+            <TimerContainer>
+              <BackgroundView color={timer.timerColor} />
+              <ProgressView
+                color={darkerColor}
+                style={{
+                  position: 'absolute',
+                  right: 0,
+                  width: `${progress * 100}%`,
+                  height: '100%',
+                  borderTopRightRadius: scale(13),
+                  borderBottomRightRadius: scale(13),
+                }}
+              />
+              <ContentWrapper>
+                <TimerHeaderWrapper>
+                  <IconboxWrapper>
+                    <IconView>{timer.icon}</IconView>
+                  </IconboxWrapper>
+                  <EnterImage
+                    source={require('../../assets/images/timerBox/enter-arrow.png')}
+                  />
+                </TimerHeaderWrapper>
+                <FoodTitleText weight="semi-bold">
+                  {timer.timerName}
+                </FoodTitleText>
+                <TimerText weight="bold">
+                  {currentTimer
+                    ? `${String(currentTimer.totalTime.minutes).padStart(
+                        2,
+                        '0',
+                      )}:${String(currentTimer.totalTime.seconds).padStart(
+                        2,
+                        '0',
+                      )}`
+                    : '00:00'}
+                </TimerText>
+              </ContentWrapper>
+            </TimerContainer>
+          </Animated.View>
+        </ContextMenu>
+      </Pressable>
     </Container>
   );
 };
